3dc1c99c4237754f11c4541cdaf7e893e29075a1
BigMistake <morgan.heijdemann@gmail.com>
2019-03-28 10:42:21 +0100

    Forgot to account for CORS.

--- bonque.py
+++ bonque.py
@@ -1,10 +1,14 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 import requests
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/check', methods=['GET'])
+@cross_origin()
 def check():
     email = request.args.get("email")
     r = requests.get("https://my-candidate-bonque.jmgapi.com/api/v1/Account/StatusCandidate?email=" + email)
